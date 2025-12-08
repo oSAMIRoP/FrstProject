9a7012a3491eb2582a826dbda81f9589d282170a
AmirMSH <88amir1388@gmail.com>
2025-12-08 21:35:07 +0330

    add menu list and icon

--- assest/main.js
+++ assest/main.js
@@ -2,3 +2,9 @@
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
